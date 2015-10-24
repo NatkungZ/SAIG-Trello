7463d457b6913a50db177ae677fb19546876392a
Natkungzeen <natkungzeen@gmail.com>
2015-10-24 01:52:41 -0400

    update main.go

--- main.go
+++ main.go
@@ -3,19 +3,79 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
-	//	"strconv"
+//	"strconv"
 	"encoding/json"
 	"log"
 	"net/http"
-	"os"
+//	"os"
 )
-
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	log.Println("Home")
 	if body, err := ioutil.ReadAll(r.Body); err==nil {
 		log.Println("Data",string(body))
 		fmt.Fprint(w, "Data",string(body))
+		// here
+		var dat map[string]interface{}
+		if err := json.Unmarshal(body, &dat); err != nil {
+			log.Println(err)
+			fmt.Fprint(w, "Data",string(body))
+			return
+		}
+		xEvent := dat["action"].(map[string]interface{})["type"]
+
+		if (dat["action"].(map[string]interface{})["data"].(map[string]interface{})["listAfter"] != nil){
+			xEvent = "listAfter"
+		}
+		if (dat["action"].(map[string]interface{})["data"].(map[string]interface{})["card"].(map[string]interface{})["closed"] == true){
+                        xEvent = "deleteCard"
+		}
+
+		fmt.Println(xEvent)
+		
+		var msg string
+		switch xEvent {
+			case "updateCard":
+				msg = fmt.Sprintf("UpdateCard in %s by @%s",dat["model"].(map[string]interface{})["name"],
+					dat["action"].(map[string]interface{})["memberCreator"].(map[string]interface{})["username"])
+			case "listAfter":
+			msg = fmt.Sprintf("Move card \"%s\" in %s from list \"%s\" to \"%s\" by @%s",dat["action"].(map[string]interface{})["data"].(map[string]interface{})["card"].(map[string]interface{})["name"],dat["model"].(map[string]interface{})["name"],dat["action"].(map[string]interface{})["data"].(map[string]interface{})["listBefore"].(map[string]interface{})["name"],dat["action"].(map[string]interface{})["data"].(map[string]interface{})["listAfter"].(map[string]interface{})["name"],
+		dat["action"].(map[string]interface{})["memberCreator"].(map[string]interface{})["username"])
+			case "createCard":
+	msg = fmt.Sprintf("Create card \"%s\" on %s Board in list \"%s\" by @%s",dat["action"].(map[string]interface{})["data"].(map[string]interface{})["card"].(map[string]interface{})["name"],dat["model"].(map[string]interface{})["name"],dat["action"].(map[string]interface{})["data"].(map[string]interface{})["list"].(map[string]interface{})["name"],
+		dat["action"].(map[string]interface{})["memberCreator"].(map[string]interface{})["username"])
+			case "deleteCard":
+	msg = fmt.Sprintf("Delete card \"%s\" on %s Board in list \"%s\" by @%s",dat["action"].(map[string]interface{})["data"].(map[string]interface{})["card"].(map[string]interface{})["name"],dat["model"].(map[string]interface{})["name"],dat["action"].(map[string]interface{})["data"].(map[string]interface{})["list"].(map[string]interface{})["name"],
+		dat["action"].(map[string]interface{})["memberCreator"].(map[string]interface{})["username"])
+			case "commentCard":
+	msg = fmt.Sprintf("Comment card \"%s\" text \"%s\" on %s Board in list \"%s\" by @%s",dat["action"].(map[string]interface{})["data"].(map[string]interface{})["card"].(map[string]interface{})["name"],dat["action"].(map[string]interface{})["data"].(map[string]interface{})["text"],dat["model"].(map[string]interface{})["name"],dat["action"].(map[string]interface{})["data"].(map[string]interface{})["list"].(map[string]interface{})["name"],
+		dat["action"].(map[string]interface{})["memberCreator"].(map[string]interface{})["username"])
+			case "addChecklistToCard":
+	msg = fmt.Sprintf("Create checklist \"%s\" in card \"%s\" on %s Board by @%s",dat["action"].(map[string]interface{})["data"].(map[string]interface{})["checklist"].(map[string]interface{})["name"],dat["action"].(map[string]interface{})["data"].(map[string]interface{})["card"].(map[string]interface{})["name"],dat["model"].(map[string]interface{})["name"],
+		dat["action"].(map[string]interface{})["memberCreator"].(map[string]interface{})["username"])
+			case "createCheckItem":
+	msg = fmt.Sprintf("Create checklist item \"%s\" in checklist \"%s\" in card \"%s\" on %s Board by @%s",dat["action"].(map[string]interface{})["data"].(map[string]interface{})["checkItem"].(map[string]interface{})["name"],dat["action"].(map[string]interface{})["data"].(map[string]interface{})["checklist"].(map[string]interface{})["name"],dat["action"].(map[string]interface{})["data"].(map[string]interface{})["card"].(map[string]interface{})["name"],dat["model"].(map[string]interface{})["name"],
+		dat["action"].(map[string]interface{})["memberCreator"].(map[string]interface{})["username"])
+			case "updateCheckItemStateOnCard":
+	msg = fmt.Sprintf("Check item \"%s\" in checklist \"%s\" in card \"%s\" on %s Board by @%s",dat["action"].(map[string]interface{})["data"].(map[string]interface{})["checkItem"].(map[string]interface{})["name"],dat["action"].(map[string]interface{})["data"].(map[string]interface{})["checklist"].(map[string]interface{})["name"],dat["action"].(map[string]interface{})["data"].(map[string]interface{})["card"].(map[string]interface{})["name"],dat["model"].(map[string]interface{})["name"],
+		dat["action"].(map[string]interface{})["memberCreator"].(map[string]interface{})["username"])
+			case "removeChecklistFromCard":
+	msg = fmt.Sprintf("Removed checklist \"%s\" in card \"%s\" on %s Board by @%s",dat["action"].(map[string]interface{})["data"].(map[string]interface{})["checklist"].(map[string]interface{})["name"],dat["action"].(map[string]interface{})["data"].(map[string]interface{})["card"].(map[string]interface{})["name"],dat["model"].(map[string]interface{})["name"],
+		dat["action"].(map[string]interface{})["memberCreator"].(map[string]interface{})["username"])
+	
+		default:
+			msg = ""
+		}
+		fmt.Println(msg)
+		
+	if msg != "" {
+		_, err := fbBot("saig2015", msg)
+		if err != nil {
+			log.Println(err)
+		}
+	} else {
+	}
+
 	}else{
 		log.Println("Home",string(r.URL.Path))
 		fmt.Fprintf(w, "Home %s",string( r.URL.Path))
@@ -23,12 +83,12 @@ func handler(w http.ResponseWriter, r *http.Request) {
 }
 
 func fbBot(group string, msg string) ([]byte, error) {
-	fbHost := os.Getenv("GO_HOST")
+	fbHost := "http://172.17.42.1:8000" // TODO:Change this
 	reqUrl := fbHost + "/" + group + "/" + msg
 	log.Println(reqUrl)
 	res, err := http.Get(reqUrl)
 	if err != nil {
-		log.Fatal(err)
+		log.Println(err)
 	}
 
 	bot, err := ioutil.ReadAll(res.Body)
@@ -39,7 +99,7 @@ func fbBot(group string, msg string) ([]byte, error) {
 
 func main() {
 	// Go Web
-	port := 9000
+	port := ":9000"
 
 	http.HandleFunc("/", handler)
 	http.ListenAndServe(port, nil)
